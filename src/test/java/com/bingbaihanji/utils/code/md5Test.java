5248c9e3c05fe3e43c35183abff48dba371bcec2
冰白寒祭 <bingbaihanji@foxmail.com>
2023-08-10 11:26:19 +0000

    删除文件 src/test/java/com/bingbaihanji/utils/code

--- src/test/java/com/bingbaihanji/utils/code/md5Test.java
+++
@@ -1,7 +0,0 @@
-package com.bingbaihanji.utils.code;
-
-public class md5Test {
-    public static void main(String[] args) {
-        System.out.println(MD5Util.getMD5("long2761868857").toUpperCase());
-    }
-}
